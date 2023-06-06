29ebb2ae4b67b9740253284c795cdd66ec006359
OlenaKhamilivska <stadni4yk.elena@gmail.com>
2023-06-06 15:26:55 +0100

    new project

---
+++ mylibrary/src/main/java/com/example/mylibrary/Show.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Show {
+
+    public static void showToast (Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
